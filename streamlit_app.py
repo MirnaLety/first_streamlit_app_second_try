59c960805cf90d5a68c3cfeed7d5f7bf9644a768
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-27 21:44:06 -0600

    Emojis + line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & and Blueberry Oatmeal')
-streamlit.text('Kale, Spinch and Rocket Smothie')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & and Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinch and Rocket Smothie')
+streamlit.text(' 🥑🍞 Hard Boiled Free Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
